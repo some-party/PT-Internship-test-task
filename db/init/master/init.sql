1ebbb2abc14e6123f66dc99f010dd41579f5faaa
some_party <113181936+some-party@users.noreply.github.com>
2024-10-16 02:26:50 +0300

    another sql update

--- db/init/master/init.sql
+++ db/init/master/init.sql
@@ -5,7 +5,12 @@ BEGIN
     END IF;
 END $$;
 
-SELECT pg_create_physical_replication_slot('replication_slot');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_replication_slots WHERE slot_name = 'replication_slot') THEN
+        PERFORM pg_create_physical_replication_slot('replication_slot');
+    END IF;
+END $$;
 
 DO $$
 BEGIN
